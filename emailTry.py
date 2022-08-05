6720078dd6b495c31a2bb3de4cf6ebc4a7c61bcc
s-vintagew <souhardyasarkar735@gmail.com>
2022-08-06 00:54:52 +0530

    email module completed

---
+++ emailTry.py
@@ -0,0 +1,14 @@
+import smtplib
+
+def sendMail():
+    fromaddr = "souhardyasarkar20@gnu.ac.in"
+    toaddr = "souhardyasarkar735@gmail.com"
+    msg = "Tu chutiya"
+    username = "souhardyasarkar20@gnu.ac.in"
+    password = "gnu15102020"
+    server = smtplib.SMTP('smtp.gmail.com:587')
+    server.starttls()
+    server.login(username,password)
+    server.sendmail(fromaddr,toaddr,msg)
+    server.quit()
+sendMail()
