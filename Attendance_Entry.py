050367587004d3585e779fdfcb80f57cbf736f53
ABEL BENEDICT <78831497+2171001@users.noreply.github.com>
2022-08-06 00:57:38 +0530

    Attendance_Entry_Complete

---
+++ Attendance_Entry.py
@@ -0,0 +1,17 @@
+import xlwt
+from xlwt import Workbook
+import datetime
+def present(rollno):
+    # Writing to an excel
+    # sheet using Python
+    current_time = datetime.datetime.now()
+
+    # Workbook is created
+    wb = Workbook()
+
+    # add_sheet is used to create sheet.
+    sheet1 = wb.add_sheet('Sheet 1')
+    sheet1.write(rollno, current_time.day, 'P')
+
+
+    wb.save('xlwt example.xls')
