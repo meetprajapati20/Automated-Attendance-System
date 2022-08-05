4ab590df8d50cce2897d39d75b1b336355905856
Souhardya Sarkar <76038553+s-vintagew@users.noreply.github.com>
2022-08-05 12:15:20 +0530

    changes to tts

--- textToSpeech.py
+++ textToSpeech.py
@@ -1,36 +1,5 @@
-import psutil
-import pyttsx3
+import pyttsx3 as pyt
 
-def func(c):
-    engine=pyttsx3.init() 
-    if(c==True):
-        engine.say("Charger Plugged")
-    else:
-        engine.say("Charger Unplugged")
-    engine.runAndWait() 
-
-
-battery=psutil.sensors_battery()
-a=battery.power_plugged
-engine=pyttsx3.init() 
-low=False
-high=False
-func(a)
-while (True):
-    x=int(battery.percent)
-    if(x==100 and high==False):
-        engine.say("Fully Charged")
-        engine.runAndWait()
-        high=True
-    elif(x==15 and low==False):
-        engine.say("Battery Low, Charge Soon")
-        engine.runAndWait()
-        low=True
-    elif(x>15 and x<100):
-        low=high=False
-    battery=psutil.sensors_battery()
-    b=battery.power_plugged
-    if not(a==b):
-        func(b)
-        a=b
-        
+engine=pyt.init()
+engine.say('Hello World')
+engine.runAndWait()
