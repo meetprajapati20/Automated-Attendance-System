f2650b701bc29da7f8472d66c6c6b0c761d95dab
s-vintagew <souhardyasarkar735@gmail.com>
2022-08-05 22:37:56 +0530

    recordaudio final change

--- recordaudio.py
+++ recordaudio.py
@@ -1,10 +1,11 @@
-def recordAudio():
+def recordAudio(text):
   import sounddevice as sd
   from scipy.io.wavfile import write
   fs = 44100  # Sample rate
   seconds = 5  # Duration of recording
-
+  text=text+".wav"
   myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
   sd.wait()  # Wait until recording is finished
-  write('output.wav', fs, myrecording)  # Save as WAV file
-recordAudio()
+  write(text, fs, myrecording)  # Save as WAV file
+
+recordAudio("Hello")
