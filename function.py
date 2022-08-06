97da525f2f5f1af3fbe2d21d946cfae93a0e0cac
ABEL BENEDICT <78831497+2171001@users.noreply.github.com>
2022-08-06 05:47:54 +0530

    ready for jury

--- function.py
+++ function.py
@@ -12,10 +12,10 @@ from xlutils.copy import copy
 from xlrd import open_workbook
 import datetime
 
-def recordAudio(text):
+def recordAudio():
   fs = 44100
   seconds = 5 
-  text="tempRecording/"+str(text)+".wav"
+  text="tempRecording/temp.wav"
   myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
   sd.wait() 
   write(text, fs, myrecording)  
@@ -24,39 +24,35 @@ def textToSpeech(text):
     engine=pyt.init()
     rate=engine.getProperty('rate')
     engine.setProperty('rate',rate-30)
-    engine.say(text)
+    engine.say("Roll Number "+str(text))
     engine.runAndWait()
 
-def voiceDiarization(inputAudioFile,listFile):
+def voiceDiarization(inputAudioFile,File):
     max_similarity = 0
     duration1, fp_encoded1 = acoustid.fingerprint_file(inputAudioFile)
     fingerprint1, version1 = chromaprint.decode_fingerprint(fp_encoded1)
-    for i in listFile:
-        duration2, fp_encoded2 = acoustid.fingerprint_file("wav/"+str(i))
-        fingerprint2, version2 = chromaprint.decode_fingerprint(fp_encoded2)
-        similarity = fuzz.ratio(fingerprint1, fingerprint2)
-        if similarity > max_similarity:
-            max_similarity = similarity
-            max_file = i
-        print(similarity,i)
-    return max_file
+    duration2, fp_encoded2 = acoustid.fingerprint_file(File)
+    fingerprint2, version2 = chromaprint.decode_fingerprint(fp_encoded2)
+    similarity = fuzz.ratio(fingerprint1, fingerprint2)
+    print(similarity,File)
+    return similarity
 
 def attandance(rollno,status):
     current_time = datetime.datetime.now()
     rb = open_workbook("Attendance_Entry.xls")
     wb = copy(rb)
     w_sheet = wb.get_sheet(0)
-    if status == rollno:
-        w_sheet.write(rollno, current_time.day, 'P')
-    else:
-        w_sheet.write(rollno, current_time.day, 'A')
+    w_sheet.write(rollno, current_time.day, status)
     wb.save('Attendance_Entry.xls')
 
 def getStudentEnroll():
     wb = open_workbook("Attendance_Entry.xls")
     w_sheet = wb.sheet_by_index(0)
     enRoll = list()
-    while (w_sheet.cell_value(i,0) != ''):
+    i=1
+    j=w_sheet.nrows
+    print(j)
+    while (i != j):
         enRoll.append(w_sheet.cell_value(i,0))
         i+=1
     return enRoll
@@ -67,11 +63,16 @@ if __name__ == "__main__" :
     nList = getStudentEnroll()
 
     for i in nList:
+        mp3="wav/"+str(int(i))+".wav"
+        textToSpeech(int(i))
         print("start recording")
-        recordAudio(i)
+        recordAudio()
         print("Audio Recorded .....")
         print("start voice diarization")
-        file = "tempRecording/"+str(i)+".wav"
-        speaker = voiceDiarization(file,listFile)
-        print(speaker.split(".")[0], "is present.")
-        attandance(i,speaker.split(".")[0])
+        file = "tempRecording/temp.wav"
+        speaker = voiceDiarization(file,mp3)
+        if(speaker>50):
+            attandance(i,"P")
+        else:
+        #print(speaker.split(".")[0], "is present.")
+            attandance(i,"A")
